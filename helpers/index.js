9a6928ae68915fd9fb0a307cf4c41de60b39c4ed
VlShost <vlshost9386@gmail.com>
2023-08-07 19:21:10 +0300

    add email verification

--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,12 @@ const HttpError = require('./HttpError');
 const ctrlWrapper = require('./ctrlWrapper');
 const handleMongooseError = require('./handleMongooseError');
 const imgHandler = require('./imgHandler');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   handleMongooseError,
   imgHandler,
+  sendEmail,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const ctrl = require('../../controllers/auth');
 const { validateBody, authenticate, upload } = require('../../middlewares');
-const { registerSchema, loginSchema, updateSubscriptionSchema } = require('../../models/user');
+const {
+  registerSchema,
+  loginSchema,
+  updateSubscriptionSchema,
+  emailSchema,
+} = require('../../models/user');
 const router = express.Router();
 
 router.post('/register', validateBody(registerSchema), ctrl.register);
+
+router.get('/verify/:verificationToken', ctrl.verifyEmail);
+
+router.post('/verify', validateBody(emailSchema), ctrl.resendVerifyEmail);
 
 router.post('/login', validateBody(loginSchema), ctrl.login);
 
